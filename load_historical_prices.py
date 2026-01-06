a46156d2b2e822108364095aea1e5c89d998b900
Bradley <bradleymathematics@gmail.com>
2026-01-06 08:58:56 -0500

    change name of fetch_price.py to load_historical_prices.py

--- load_historical_prices.py
+++ load_historical_prices.py
@@ -39,7 +39,7 @@ def main():
             for date, row in hist.iterrows():
                 try:
                     cur.execute("""
-                        INSERT INTO fact_stock_prices_daily
+                        INSERT INTO daily_stock_prices 
                         (symbol, date, open, high, low, close, volume, dividends, stock_splits)
                         VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
                         ON CONFLICT (symbol, date) DO NOTHING
